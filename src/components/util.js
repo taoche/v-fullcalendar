ad20f7b3455accf9d73738e29d5908d9d29b0dab
taoche <taoche2010@gmail.com>
2019-04-04 11:59:11 +0800

    fix: util function argument error

--- src/components/util.js
+++ src/components/util.js
@@ -1,5 +1,5 @@
-export function assert(condition, message) {
-  if (process.env.NODE_ENV !== "production" && !condition) {
+export function assert(message) {
+  if (process.env.NODE_ENV !== "production") {
     // useful when testing
     // console.warn(`[v-fullcalendar] ${message}`)
     throw new Error(`[v-fullcalendar] ${message}`);
